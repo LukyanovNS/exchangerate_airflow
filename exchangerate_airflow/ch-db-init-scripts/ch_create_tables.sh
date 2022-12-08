8084489b0bab4c14dbc0f62a6d761ebe29c93b8c
LukyanovNS <101443096+LukyanovNS@users.noreply.github.com>
2022-12-08 20:21:59 +0700

    Update ch_create_tables.sh

--- exchangerate_airflow/ch-db-init-scripts/ch_create_tables.sh
+++ exchangerate_airflow/ch-db-init-scripts/ch_create_tables.sh
@@ -1,3 +1,3 @@
 clickhouse-client --query="CREATE DATABASE IF NOT EXISTS exchangerate_db"
 clickhouse-client --database=exchangerate_db --query="CREATE TABLE IF NOT EXISTS exchangerate_landing (CreateDTime DateTime, RateDate date, response String) ENGINE=MergeTree() ORDER BY CreateDTime"
-clickhouse-client --database=exchangerate_db --query="CREATE TABLE IF NOT EXISTS exchangerate_parsed (CreateDTime DateTime, RateDate date, CurrencyPair String, Rate float) ENGINE=MergeTree() ORDER BY CreateDTime"
+clickhouse-client --database=exchangerate_db --query="CREATE TABLE IF NOT EXISTS exchangerate_parsed (CreateDTime DateTime, RateDate date, CurrencyPair String, Rate float, INDEX IX_RateDate RateDate TYPE minmax GRANULARITY 8192) ENGINE=MergeTree() ORDER BY CreateDTime"
